d4990cb1899474cd1633522140abbd76691988e7
menna kh <fattoshak@gmail.com>
2026-07-28 02:53:53 +0300

    Delete js/task1minmax/plugin.js

--- js/task1minmax/plugin.js
+++
@@ -1,9 +0,0 @@
-//percentage of subjects 
-let biology = Number(prompt("enter biology degree of 20"));
-let Computer = Number(prompt("enter Computer degree  of 20"));
-let Chemistry = Number(prompt("enter biology Chemistry degree  of 20"));
-let math = Number(prompt("enter math degree of 20"));
-let physics = Number(prompt("enter physics  degree of 20"));
-let total = biology + Computer + Chemistry + math + physics;
-let percentage = total / 100;
-if (percentage >= .90) { alert(`Grade A`) } else if (percentage >= .80) { alert(`Grade B`) } else if (percentage >= .70) { alert(`Grade C`) } else if (percentage >= .60) { alert(`Grade D`) } else if (percentage >= .40) { alert(`Grade E`) } else { alert(`Grade F`) };
